fbb709b882e53408c6346962f0ec0bb29d2f37c9
oyushik <craven_sawdust.9i@icloud.com>
2025-07-12 00:04:44 +0900

    fix/29-docker-compose-available

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # 프론트엔드 Origin 허용
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost/"],  # 프론트엔드 Origin 허용
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
